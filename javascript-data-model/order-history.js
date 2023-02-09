42747125e627ffcfcb08b43887249df8ed52a727
Holly Phung <hollyphung95@gmail.com>
2023-02-09 19:26:31 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,37 @@
+var orderHistory = [
+  {
+    order: { number: '114-3941689-8772232', date: 'August 4, 2020', total: 34.00 },
+    product: { type: 'Book', description: ['JavaScript for impatient programmers', 'Rauschmayer, Dr. Axel'], total: 31.55 },
+    shipping: { status: 'Closed', name: 'JS Masher' },
+    delivery: { status: 'Closed', date: 'Aug 8, 2020' },
+    return: { status: 'Closed', date: 'Sep 7, 2020' }
+  },
+  {
+    order: { number: '113-9984268-1280257', date: 'July 19, 2020', total: 44.53 },
+    product: { type: 'Book', description: ['The Timeless Way of Building', 'Alexander, Christopher'], total: 41.33 },
+    shipping: { status: 'Closed', name: 'JS Masher' },
+    delivery: { status: 'Closed', date: 'Jul 20, 2020', description: 'Your package was delivered. It was handed directly to a resident.' },
+    return: { status: 'Closed', date: 'Aug 19, 2020' }
+  },
+  {
+    order: { number: '114-2875557-9059409', date: 'July 4, 2020', total: 17.22 },
+    product: { type: 'Gaming', description: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter', total: 15.98 },
+    shipping: { status: 'Closed', name: 'JS Masher' },
+    delivery: { status: 'Closed', date: 'Jul 7, 2020', description: 'Your package was delivered. It was handed directly to a resident.' },
+    return: { status: 'Closed', date: 'Aug 5, 2020' }
+  },
+  {
+    order: { number: '113-2883177-2648248', date: 'July 3, 2020', total: 138.93 },
+    product: { type: 'Gaming', description: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', total: 94.95 },
+    shipping: { status: 'Closed', name: 'JS Masher' },
+    delivery: { status: 'Closed', date: 'Jul 5, 2020' },
+    return: { status: 'Closed', date: 'Aug 4, 2020' }
+  },
+  {
+    order: { number: '113-2883177-2648248', date: 'July 3, 2020', total: 138.93 },
+    product: { type: 'Book', description: ['The Art of Sql', 'Faroult, Stephane'], total: 33.99 },
+    shipping: { status: 'Closed', name: 'JS Masher' },
+    delivery: { status: 'Closed', date: 'Jul 5, 2020' },
+    return: { status: 'Closed', date: 'Aug 4, 2020' }
+  }
+];
