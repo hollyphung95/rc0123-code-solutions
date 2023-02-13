1aca81dfe359d7785c084ff770ab6552fa7bf49f
Holly Phung <hollyphung95@gmail.com>
2023-02-13 10:26:16 +0000

    Completed javascript-view-swapping exercise

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,32 +1,17 @@
 var $tabContainer = document.querySelector('.tab-container');
-var $tabNodeList = document.querySelectorAll('.tab');
-var $viewContainer = document.querySelector('.view-container');
-var $viewNodeList = document.querySelectorAll('.view');
+var $tabElements = document.querySelectorAll('.tab');
+var $viewElements = document.querySelectorAll('.view');
 
-for (var i = 0; i < $tabNodeList.length; i++) {
-  $tabContainer.append($tabNodeList[i]);
-  $viewContainer.append($viewNodeList[i]);
-}
-
-$tabContainer.addEventListener('click', handleClick);
-function handleClick(event) {
-  var dataViewAttr = event.target.getAttribute('data-view');
-
-  for (var i = 0; i < $tabNodeList.length; i++) {
-    $tabNodeList[i].className = 'tab';
-    $viewNodeList[i].className = 'view hidden';
+$tabContainer.addEventListener('click', function (event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $tabElements.length; i++) {
+      if ($tabElements[i] === event.target) {
+        $tabElements[i].className = 'tab active';
+        $viewElements[i].className = 'view';
+      } else {
+        $tabElements[i].className = 'tab';
+        $viewElements[i].className = 'view hidden';
+      }
+    }
   }
-  if (dataViewAttr === 'html') {
-    $tabNodeList[0].className = 'tab active';
-    $viewNodeList[0].className = 'view';
-  } else if (dataViewAttr === 'css') {
-    $tabNodeList[1].className = 'tab active';
-    $viewNodeList[1].className = 'view';
-  } else if (dataViewAttr === 'javascript') {
-    $tabNodeList[2].className = 'tab active';
-    $viewNodeList[2].className = 'view';
-  } else {
-    $tabNodeList[0].className = 'tab active';
-    $viewNodeList[0].className = 'view';
-  }
-}
+});
