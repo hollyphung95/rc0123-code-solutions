88fed9490b73cc181fe77e73b90f0e4cbf4fb60a
Holly Phung <hollyphung95@gmail.com>
2023-02-21 18:27:03 +0000

    javascript-constructors Completed

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,32 @@
+/* complete the following tasks for each of these built-in JavaScript constructors:
+ Object, Array, Function, String, Number, and Boolean:
+  Call the constructor with new and assign the return value to a variable. */
+var anObject = new Object();
+
+/* Log the value of the instance to the console. */
+console.log('value of anObject:', anObject);
+
+/* Log the typeof the instance to the console. */
+console.log('typeof anObject:', typeof anObject);
+
+/* repeat for Array, Function, String, Number, and Boolean. */
+
+var anArray = new Array();
+console.log('value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
+
+var aFunction = new Function();
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction:', typeof aFunction);
+
+var aString = new String();
+console.log('value of aString:', aString);
+console.log('typeof aString:', typeof aString);
+
+var aNumber = new Number();
+console.log('value of aNumber:', aNumber);
+console.log('typeof aNumber:', typeof aNumber);
+
+var myBool = new Boolean();
+console.log('value of myBool:', myBool);
+console.log('typeof myBool:', typeof myBool);
