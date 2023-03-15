042d445c69fcc7969c11db1e8830bfa77e455e43
Holly Phung <hollyphung95@gmail.com>
2023-03-15 23:39:23 +0000

    recommiting trial 4

--- array-reduce/main.js
+++ array-reduce/main.js
@@ -16,3 +16,22 @@ const traits = [
   { level: 15 },
   { trainer: 'ash' }
 ];
+
+/* main.js, create the following variables using Array.reduce and log each of them to the console.
+sum - all numbers added together
+product - all numbers multiplied together
+balance - a number that is the result of combining all transactions in account. The correct answer is 180.
+composite - an object that is the result of combining all properties of the objects within traits.
+composite should come out like this (property order doesn't matter): */
+const sum = numbers.reduce((total, number) => {
+  return total + number;
+}, 0);
+console.log('reduced the array to the value sum: ', sum);
+
+const product = numbers.reduce((total, number) => total * number);
+console.log('reduced the array to the value product: ', product);
+
+const balance = account.reduce((total, currValue) => { currValue.type === 'deposit' ? total += currValue.amount : total -= currValue.amount; return total; }, 0);
+console.log('reduced the list of transactions to the total balance: ', balance);
+const composite = traits.reduce((result, trait) => Object.assign(result, trait), {});
+console.log('reduced the list of traits to a groupedresult value: ', composite);
