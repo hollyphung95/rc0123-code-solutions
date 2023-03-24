a5bbb7f43a373a0dc5ef1a0d5559be4f63ed2ea3
Holly Phung <hollyphung95@gmail.com>
2023-03-24 03:48:04 +0000

    completed node-module exercise

---
+++ node-module-system/add.js
@@ -0,0 +1,10 @@
+
+/**
+ * @function  add x and y together.
+ * @param {number} x - The value on the left of the operator.
+ * @param {number} y - The value on the right of the operator.
+ * @return {number} sum - The sum of x plus y.
+ */
+export default function add(x, y) {
+  return x + y;
+}
